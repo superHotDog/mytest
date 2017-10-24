aaea03e51fcf29347a55230da41ea29beda5be2d
mww <1353669924@qq.com>
2017-10-24 22:57:21 +0800

    ddf

--- demo-test/src/cn/itcast/Hello.java
+++ demo-test/src/cn/itcast/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("1111111");
+		System.out.println("22222222");
+		System.out.println("6666666");
 	}
 }
